0180ff2b2cdeaba75aa4c00c4fb6d1ff73feb7e2
steffenhvid <steffenhvid@gmail.com>
2018-06-10 11:56:43 +0200

    Added a method for generating random data

--- LinearClassification.py
+++ LinearClassification.py
@@ -1,3 +1,12 @@
+import numpy as np
+
 
 class LinearClassification:
     pass
+
+    def generate_random_data(self, n):
+        '''
+        :param n: Size of dataset to return
+        :return: A 2d linearly seperable data set.
+        '''
+        return np.concatenate((np.random.random_integers(0,10, (int(n/2), 2)),np.random.random_integers(11,20, (int(n/2), 2))))
