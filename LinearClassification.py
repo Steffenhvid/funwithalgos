6f67ef461e1cd9d52c1c8d31f6d12651b1281081
steffenhvid <steffenhvid@gmail.com>
2018-06-12 09:34:58 +0200

    Added inheritance from SupervisedLearning

--- LinearClassification.py
+++ LinearClassification.py
@@ -1,12 +1,7 @@
+from SupervisedLearning import SupervisedLearning
 import numpy as np
 
 
-class LinearClassification:
+class LinearClassification(SupervisedLearning):
     pass
 
-    def generate_random_data(self, n):
-        '''
-        :param n: Size of dataset to return
-        :return: A 2d linearly seperable data set.
-        '''
-        return np.concatenate((np.random.random_integers(0,10, (int(n/2), 2)),np.random.random_integers(11,20, (int(n/2), 2))))
