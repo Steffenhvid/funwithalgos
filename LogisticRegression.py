eb8ab15561d8897625f4e44108d5eb5245065ce3
steffenhvid <steffenhvid@gmail.com>
2018-06-16 15:28:29 +0200

    Added the cost function

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,50 +5,29 @@ import numpy as np
 class LogisticRegression(SupervisedLearning):
     pass
 
-    def sigmoid(self, z):
+    def sigmoid(self, X, theta):
         '''
-        Computes the sigmoid given a the signal
-        :param z: numpy array shape (d,)
-        :return: z transformed by the sigmoid function
+        Computes the sigmoid given the data and the weights
+        :param X: numpy array shape (n,d)
+        :param theta: numpy array shape (d,1)
+        :return: z transformed by the sigmoid function (signal)
         '''
+        z = X.dot(theta)
         sig = 1/(1+np.exp(-z))
         assert sig.shape == z.shape
         return sig
 
-    def cost_function(self, X, y, w, reg=0):
+    def cost_function(self, X, y, theta):
         '''
-        Compute the (regularized) cross entropy and the gradient under the logistic regression model
-        using data X, targets y, weight vector w (and regularization reg)
-
-        The L2 regularization is 1/2 reg*|w_{1,d}|^2 i.e. w[0], the bias, is not regularized
         :param X: np.array shape (n,d)
-        :param y: np.array shape (n,)
-        :param w: np.array shape (d,)
+        :param y: np.array shape (n,1)
+        :param theta: np.array shape (d,1)
         :param reg: scalar - regularization parameter
         :return:
-            cost: scalar the cross entropy cost of logistic regression with data X,y using regularization parameter reg
-            grad: np.arrray shape(n,d) gradient of cost at w with regularization value reg
+            cost:
         '''
-        cost = 0
-        # Computing the cost
-        z = np.dot(w, X.T)
-        logs = self.sigmoid(z)
-        ln_log1 = np.log(logs)
-        ln_log2 = np.log(1 - logs)
-        fp = y * ln_log1
-        sp = (1 - y) * ln_log2
-        sums = np.sum(fp + sp)
-        cost -= (1 / len(y)) * sums
-        cost += (1 / 2) * reg * (len(w) - 1) ** 2
-
-        # Computing the gradient
-        z2 = np.dot(X, w)
-        logs2 = self.sigmoid(z2)
-        inner = y - logs2
-        prod = np.dot(-X.T, inner)
-        grad = (1 / len(y)) * prod
-        grad += reg * w * (1 / len(y))
-        ### END CODE
-        assert grad.shape == w.shape
-        return cost, grad
+        z = self.sigmoid(X, theta)
+        n = len(z)
+        cost = -sum(y * np.log(z) + (1-y) * np.log(1-z))/n
+        return cost
 
